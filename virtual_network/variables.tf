c909822278fd8150b79c78bd37db410054c9c331
ppaliwal85 <ppaliwal85@outlook.com>
2022-12-18 13:12:49 +0530

    committing vnet.tf file

---
+++ virtual_network/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+    default = "RG100"
+    description = "Default resource group name"
+}
+
+variable "resource_group_location" {
+    default = "Central India"
+    description = "Default location"
+}
